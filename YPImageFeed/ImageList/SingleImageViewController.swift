3bafbfeaee16e6cc1141ec0db54499391419e61b
Meyoq <oleg.a.97@icloud.com>
2024-12-02 15:26:10 +0300

    Добавлен NavigationController

---
+++ YPImageFeed/ImageList/SingleImageViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SingleImageViewController.swift
+//  YPImageFeed
+//
+//  Created by Олег Дерябин on 02.12.2024.
+//
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+}
